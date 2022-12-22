fa35605cfda9d581e33f35500816d6d607806bf1
Emdorka <emdorkasyiem1@gmail.com>
2022-12-23 00:43:06 +0530

    ADDED: code to print monthly wages

--- WageComputation.java
+++ WageComputation.java
@@ -2,27 +2,34 @@ package com.bridgelabz.empwage;
 
 public class WageComputation {
     public static void main(String[] args) {
-        final int WAGE_PER_HOUR = 20;
-        final int WORKING_HOUR = 8;
-        final int PART_TIME = 4;
-        int dailyWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        System.out.println(empCheck);
-        switch (empCheck) {
-            case 1: {
-                System.out.println("Employee is present ");
-                dailyWage = WORKING_HOUR * WAGE_PER_HOUR;
+        int empHrs;
+        int dailyWage;
+        int monthlyWage = 0;
+        int wagePerHr = 20;
+        int workingDays = 20;
+        for (int i = 0; i < workingDays; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println(empCheck);
+            switch (empCheck) {
+                case 1: {
+                    System.out.println("Employee is present ");
+                    empHrs = 8;
+                    break;
+                }
+                case 2: {
+                    System.out.println(" Employee is part - time ");
+                    empHrs = 4;
+                    break;
+                }
+                case 0: {
+                    System.out.println(" Employee is Absent ");
+                    empHrs = 0;
+                }
+                dailyWage = empHrs * wagePerHr;
+                monthlyWage = dailyWage * workingDays;
+                System.out.println(" daily wages is " + dailyWage);
             }
-             break;
-            case 2: {
-                System.out.println(" Employee is part - time ");
-                dailyWage = WORKING_HOUR * PART_TIME;
-            }
-            break;
-            case 0: {
-                System.out.println(" Employee is Absent " );
-            }
+            System.out.println("Employee daily wage is " + monthlyWage);
         }
-        System.out.println( "Employee daily wage is " + dailyWage);
     }
 }
