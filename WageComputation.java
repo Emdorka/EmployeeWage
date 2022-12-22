0c2bff3bbe6642bcbe34494ab4d7e8596c53dfb0
Emdorka <emdorkasyiem1@gmail.com>
2022-12-22 09:23:24 +0530

    ADDED: code to check employee present or not

--- WageComputation.java
+++ WageComputation.java
@@ -3,5 +3,12 @@ package com.bridgelabz.empwage;
 public class WageComputation {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage computation program");
+        double empCheck =  Math.floor(Math.random() * 10) % 2;
+        if (empCheck == 1) {
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
     }
 }
