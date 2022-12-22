c98b1331e5c5e6cdf62412a126d5857deef0f651
Emdorka <emdorkasyiem1@gmail.com>
2022-12-22 16:46:50 +0530

    ADDED: code to check part time

--- WageComputation.java
+++ WageComputation.java
@@ -4,15 +4,18 @@ public class WageComputation {
     public static void main(String[] args) {
         final int WAGE_PER_HOUR = 20;
         final int WORKING_HOUR = 8;
-        int dailyWage = 0 ;
-        double empCheck =  Math.floor(Math.random() * 10) % 2;
+        final int PART_TIME = 4;
+        int dailyWage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == 1) {
-            //System.out.println("Employee is present " + dailyWage);
             dailyWage = WAGE_PER_HOUR * WORKING_HOUR;
-            System.out.println("Employee is present " + dailyWage);
-        }
-        else{
-            System.out.println("Employee is absent");
+            System.out.println("Employee is present " );
+            } else if ( empCheck == 2) {
+            System.out.println(" Employee is part - time ");
+            dailyWage = WORKING_HOUR * PART_TIME;
+        } else {
+                System.out.println("Employee is absent");
+            }
+        System.out.println(" Employee daily wage is " + dailyWage);
         }
     }
-}
