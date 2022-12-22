3c129156a90b61fb438353197220e6f0f9ac7e63
Emdorka <emdorkasyiem1@gmail.com>
2022-12-22 09:41:15 +0530

    ADDED: to calculate daily wage

--- WageComputation.java
+++ WageComputation.java
@@ -2,10 +2,14 @@ package com.bridgelabz.empwage;
 
 public class WageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation program");
+        final int WAGE_PER_HOUR = 20;
+        final int WORKING_HOUR = 8;
+        int dailyWage = 0 ;
         double empCheck =  Math.floor(Math.random() * 10) % 2;
         if (empCheck == 1) {
-            System.out.println("Employee is present");
+            //System.out.println("Employee is present " + dailyWage);
+            dailyWage = WAGE_PER_HOUR * WORKING_HOUR;
+            System.out.println("Employee is present " + dailyWage);
         }
         else{
             System.out.println("Employee is absent");
