eec0c50a741b5f647ce76f3e4aaeaf2f1e9e8845
Emdorka <emdorkasyiem1@gmail.com>
2022-12-22 11:17:36 +0530

    ADDED: code to print the daily wage per hour

--- WageComputation.java
+++ WageComputation.java
@@ -6,11 +6,9 @@ public class WageComputation {
         final int  DAILY_PER_HR = 20;
         int dailyWage = 0;
         double empCheck =  Math.floor(Math.random() * 10) % 2;
-        //dailyWage = WORKING_HR * DAILY_PER_HR;
         if (empCheck == 1) {
             dailyWage = WORKING_HR * DAILY_PER_HR;
             System.out.println("Employee is present " + dailyWage);
-            //dailyWage = WORKING_HR * DAILY_PER_HR;
         }
         else{
             System.out.println("Employee is absent");
