be0cce60f4ecbef265a2a8a3a6662ee8e8a344d9
Emdorka <emdorkasyiem1@gmail.com>
2022-12-25 20:10:57 +0530

    ADDED: code to calculate montly wage for a certain condition

--- WageComputation.java
+++ WageComputation.java
@@ -3,33 +3,37 @@ package com.bridgelabz.empwage;
 public class WageComputation {
     public static void main(String[] args) {
         int empHrs;
-        int dailyWage;
         int monthlyWage = 0;
-        int wagePerHr = 20;
-        int workingDays = 20;
-        for (int i = 0; i < workingDays; i++) {
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            System.out.println(empCheck);
-            switch (empCheck) {
-                case 1: {
-                    System.out.println("Employee is present ");
-                    empHrs = 8;
-                    break;
+        final int wagePerHr = 20;
+        final int workingDays = 20;
+        final int workingHr = 100;
+        int totalWorkingHr = 0;
+        int totalWorkingDay = 0;
+        while (totalWorkingDay < workingDays && totalWorkingHr < workingHr) {
+            totalWorkingDay++;
+           {
+                int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                System.out.println(empCheck);
+                switch (empCheck) {
+                    case 1: {
+                        System.out.println("Employee is present ");
+                        empHrs = 8;
+                        break;
+                    }
+                    case 2: {
+                        System.out.println(" Employee is part - time ");
+                        empHrs = 4;
+                        break;
+                    }
+                        default:
+                            System.out.println(" Employee is Absent ");
+                            empHrs = 0;
+                    }
+                    totalWorkingHr = empHrs + totalWorkingHr;
+                    System.out.println("working day " + totalWorkingDay + "  and hour is " + empHrs);
                 }
-                case 2: {
-                    System.out.println(" Employee is part - time ");
-                    empHrs = 4;
-                    break;
-                }
-                case 0: {
-                    System.out.println(" Employee is Absent ");
-                    empHrs = 0;
-                }
-                dailyWage = empHrs * wagePerHr;
-                monthlyWage = dailyWage * workingDays;
-                System.out.println(" daily wages is " + dailyWage);
+                int totalWagesForMonth = totalWorkingHr * wagePerHr;
+                System.out.println(" Monthly employee wage " + totalWagesForMonth);
             }
-            System.out.println("Employee daily wage is " + monthlyWage);
         }
     }
-}
